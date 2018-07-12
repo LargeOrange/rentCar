4637a0a6781ff80296c3b7a0ccf6f4e614d19620
LargeOrange <871762360@qq.com>
2018-07-12 10:20:59 +0800

    feat: change link

--- views/js/homePage.js
+++ views/js/homePage.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $("#login").click(function(){
-    window.location.href="http://localhost:3000/login";
+    window.location.href="http://39.106.117.29:3000//login";
     window.event.returnValue = false;
   });
   $("#addCar").click(function(){
-    window.location.href="http://localhost:3000/addCar";
+    window.location.href="http://39.106.117.29:3000//addCar";
     window.event.returnValue = false;
   });
 });
